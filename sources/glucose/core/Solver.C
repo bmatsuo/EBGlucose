fe653480fcf5a67e4ef5109a15400ba066245dda
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-02-27 21:23:41 -0800

    Double-bump of both bi-asserting literals' variables.

--- sources/glucose/core/Solver.C
+++ sources/glucose/core/Solver.C
@@ -907,8 +907,10 @@ lbool Solver::search(int nof_conflicts, int nof_learnts)
                     //assert(value(learnt_clause[0]) == l_Undef);
                     //newDecisionLevel();
                     //uncheckedEnqueue(learnt_clause[0]);
-                    //varBumpActivity(var(learnt_clause[0]));
-                    //varBumpActivity(var(learnt_clause[1]));
+                    varBumpActivity(var(learnt_clause[0]));
+                    varBumpActivity(var(learnt_clause[0]));
+                    varBumpActivity(var(learnt_clause[1]));
+                    varBumpActivity(var(learnt_clause[1]));
                 } 
                 else {
 	                uncheckedEnqueue(learnt_clause[0], c);
