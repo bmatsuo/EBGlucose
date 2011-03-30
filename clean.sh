69b6aa30297002cf8eaec5bb2349f22fbe1addb4
Bryan Matsuo <bryan.matsuo@gmail.com>
2011-03-29 23:00:33 -0700

    Fix the clean.sh script so that it gets all the generated files.

--- clean.sh
+++ clean.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 export FM=$PWD/sources/SatElite/ForMani
-rm -rf ebglucose_static 
+rm -rf ebglucose_static
 rm -rf SatELite_release
 
 cd sources/SatElite/SatELite
-make clean 
-
-cd ../../glucose/core
+make clean
+cd ../ForMani/Global
+make clean
+cd ../ADTs
+make clean
+cd ../../../glucose/core
 make clean 
  
